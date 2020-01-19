2389363355998b725759d8754354d564b43fdf6b
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-01-19 00:11:43 -0600

    root url

--- aaaimxemail/urls.py
+++ aaaimxemail/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ]
 
 # Static files urls development
